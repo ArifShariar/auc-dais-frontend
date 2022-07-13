2a9543b134b3dd576070c78945aa2666ed50d11b
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2022-07-13 20:31:43 +0600

    Added navbar, carousel and table. table not showing: fix needed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import {Col, Row} from "react-bootstrap";
+import Carousels from "./components/Carousel";
+import searchResults from "./components/SearchResults";
 function App() {
+    const marginTop = {
+        marginTop: "10px"
+    };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <NavBar />
+        <Container>
+            <Row>
+                <Col lg={12} style={marginTop}>
+                    <Carousels />
+                </Col>
+
+            </Row>
+        </Container>
+        <searchResults />
+
     </div>
   );
 }
