3ffc2cc4ec294a4bec1d7db2e3954466ce70283d
Soham Khisa <1705120@ugrad.cse.buet.ac.bd>
2022-07-25 03:52:02 +0600

    The login works properly, react-toastify After login now the login page redirects to the homepage. react-toastify package has been installed for a responsive error notification.

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,7 +1,10 @@
 import React from "react";
-import {Card} from "react-bootstrap";
+import {Card, Form, Toast} from "react-bootstrap";
 import axios from "axios";
-class LogInPage extends React.Component{
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+class LogInPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = this.initialState;
@@ -15,26 +18,48 @@ class LogInPage extends React.Component{
 
     submitForm = event => {
         event.preventDefault();
-        const user = {
-            email: this.state.email,
-            password: this.state.password
-        }
-
-        axios.post('http://localhost:8080/users/login', user).then(
+        axios.post('http://localhost:8080/users/login', {email:this.state.email, password:this.state.password}).then(
             response => {
                 if(response.data != null) {
-                    this.setState(this.initialState);
-                    alert("User Creation Successful");
+                    if(response.status == 200) {
+                        this.setState(this.initialState);
+                        alert("It's working!");
+                        window.location.replace("http://localhost:3000?id=" + response.data.id);
+                    }
                 }
             }
         ).catch(error => {
         console.log(error.response)
             // check if the error code is 5**
             if (error.response.status >= 500) {
-                alert("Server Error: Failed to create user, please try with different credentials");
+                alert("Server Error: Failed to login, please try with different credentials");
             }
-    });
+            else if (error.response.status == 400) {
+                this.notify();
+            }
+        });
     }
+
+    notify = () => {
+        toast.error('Wrong email or password',
+            {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: true,
+                closeOnClick: false,
+                pauseOnHover: false,
+                draggable: false,
+                progress: undefined,
+            },
+        );
+    }
+
+    userChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
     render() {
         return (
             <div className="card-container">
@@ -44,30 +69,29 @@ class LogInPage extends React.Component{
                         <Card className=" bg-warning.bg-gradient">
                             <Card.Header className={"bg-warning text-white text-center"}> Log In</Card.Header> 
                                 <Card.Body>
-                                    <div> 
-                                        <form>
-                                            <div className="form-group">
-                                                <label htmlFor="exampleInputEmail1">Email address</label>
-                                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-                                            </div>
-                                            <div className="form-group">
-                                                <label htmlFor="exampleInputPassword1">Password</label>
-                                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-                                            </div>
-                                            <div className="form-group d-grid gap-2 col-6 mx-auto text-container">
-                                                <button type="submit" className="btn btn-secondary">Submit</button>
-                                            </div>
+                                    <Form id="signupform" onSubmit={this.submitForm}>
+                                        <Form.Group className="mb-3" controlId="inputEmail">
+                                            <Form.Label>Email</Form.Label>
+                                            <Form.Control required value={this.state.email} onChange={this.userChange} type="email" name="email" placeholder="Email" />
+                                        </Form.Group>
 
-                                        </form>
-                                    </div>
+                                        <Form.Group className="mb-3" controlId="inputPassword">
+                                            <Form.Label>Password</Form.Label>
+                                            <Form.Control required value={this.state.password} onChange={this.userChange} type="password" name="password" placeholder="Password" />
+                                        </Form.Group>
+
+                                        <div className="form-group d-grid gap-2 col-6 mx-auto text-container">
+                                            <button type="submit" className="btn btn-secondary">Submit</button>
+                                        </div>
+                                    </Form>
 
                                 </Card.Body>
                             </Card>
                         </div>
                     </div>
                 </div>
+                <ToastContainer/>
             </div>
-            
         );
     }
 }
