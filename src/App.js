d8575872972eb3c5761f62012a3e275ae97b5538
Soham Khisa <1705120@ugrad.cse.buet.ac.bd>
2022-08-07 20:21:43 +0600

    trying out the chatroom

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import { RequireAuth } from './components/RequireAuth';
 import Message from "./components/Message";
 import Profile from "./components/Profile";
 import AllMessages from "./components/AllMessages";
+import ChatRoom from "./components/ChatRoom";
 
 // We may need to get rid of RequireAuth and use PersistLogin instead
 toast.configure();
@@ -46,6 +47,7 @@ function App() {
             <Route path="/messages" element={<AllMessages/>}/>
             <Route path="/profile" element={<RequireAuth><Profile/></RequireAuth>}/>
             <Route path="/" element={<Home />} />
+            <Route path="/chatroom" element={<RequireAuth><ChatRoom/></RequireAuth>} />
         </Routes>
     </Router>
     </AuthProvider>
