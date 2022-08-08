bd3d73460994b6231f706eacc64fb039680ef4d8
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2022-08-08 17:03:07 +0600

    Changed SavedAuctions.js from class based to functional

--- src/components/SavedAuctions.js
+++ src/components/SavedAuctions.js
@@ -1,107 +1,99 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Card} from "react-bootstrap";
-import Table from 'react-bootstrap/Table';
+import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
-import './Card.css'
 
-import {useHistory} from "react-router-dom";
+function SavedAuctions() {
+    let [savedAuctions, setSavedAuctions] = useState([]);
+    let user_id = localStorage.getItem('user_id');
+    let user_token = localStorage.getItem('user');
 
-// this class will show the saved auctions by a user
-
-class SavedAuctions extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            saved_auctions: [],
-            user_id: localStorage.getItem('user_id'),
-            user_token: localStorage.getItem('user')
-        }
-    }
-
-    componentDidMount() {
-        axios.get("http://localhost:8080/savedAuctions/get/user/" + this.state.user_id + "/" + this.state.user_token)
-            .then(response =>response.data)
-            .then((data)=>{
-                this.setState({saved_auctions: data});
-            })
-    }
-
-    deleteAuction(id) {
+    const deleteAuction = (id) => {
         // /delete/user/{userId}/auction/{auctionId}/{token}
-        let delete_url = "http://localhost:8080/savedAuctions/delete/user/" + this.state.user_id + "/auction/" + id + "/" + this.state.user_token;
+        let delete_url = "http://localhost:8080/savedAuctions/delete/user/" + localStorage.getItem('user_id') + "/auction/" + id + "/" + localStorage.getItem('user');
         axios({
             method: 'delete',
             url: delete_url
         }).then(() => {
-            this.componentDidMount();
+
         })
     }
 
-    showAuctionDetails(id) {
-        // use navigate to go to the auction details page
-        // TODO: ADD ROUTING HERE
 
+    const showAuctionDetails = (id) => {
+        // use navigate to go to the auction details page
+        alert("View Auction Details : " + id);
     }
 
-    render() {
+    useEffect(() => {
+        axios.get("http://localhost:8080/savedAuctions/get/user/" + user_id + "/" + user_token)
+            .then(response =>response.data)
+            .then((data)=>{
+                setSavedAuctions = data;
+                console.log("data : " );
+                console.log(savedAuctions);
+            })
+    })
 
-        return (
-            <div className="home-element-padding">
-                <div className="card-container ">
-                    <div className='container-fluid' >
-                        <div className="row">
-                            <div className=" col-sm-12">
-                                <Card className=" bg-warning.bg-gradient">
-                                    <Card.Header className={"bg-warning text-white text-center"}> Saved Auctions </Card.Header>
-                                    <Card.Body>
-                                        <Table bordered hover striped responsive>
-                                            <thead>
+    return(
+        <div className="home-element-padding">
+            <div className="card-container ">
+                <div className='container-fluid' >
+                    <div className="row">
+                        <div className=" col-sm-12">
+                            <Card className=" bg-warning.bg-gradient">
+                                <Card.Header className={"bg-warning text-white text-center"}> Saved Auctions </Card.Header>
+                                <Card.Body>
+                                    <Table bordered hover striped responsive>
+                                        <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th className={"text-center"}>Auction Product</th>
+                                            <th className={"text-center"}>Owner</th>
+                                            <th className={"text-center"}>Max Bid</th>
+                                            <th className={"text-center"}>Start Date</th>
+                                            <th className={"text-center"}>End Date</th>
+                                            <th colSpan={2} className={"text-center"}>Actions</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        {savedAuctions.length === 0 ?
                                             <tr>
-                                                <th>#</th>
-                                                <th className={"text-center"}>Auction Product</th>
-                                                <th className={"text-center"}>Owner</th>
-                                                <th className={"text-center"}>Max Bid</th>
-                                                <th className={"text-center"}>Start Date</th>
-                                                <th className={"text-center"}>End Date</th>
-                                                <th colSpan={2} className={"text-center"}>Actions</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            {this.state.saved_auctions.length === 0 ?
-                                                <tr>
-                                                    <td colSpan={7} className={"text-center"}>No saved auctions</td>
-                                                </tr> :
-                                                this.state.saved_auctions.map((auction, index) => {
-                                                    return (
-                                                        <tr key={auction.auctionProduct.id}>
-                                                            <td>{index + 1}</td>
-                                                            <td>{auction.auctionProduct.product_name}</td>
-                                                            <td>{auction.auctionProduct.owner.firstName}</td>
-                                                            <td>{auction.auctionProduct.max_bid}</td>
-                                                            <td>{auction.auctionProduct.auction_start_date}</td>
-                                                            <td>{auction.auctionProduct.auction_end_date}</td>
-                                                            <td>
-                                                                <Button variant="outline-danger" size="sm" className={"text-center"} onClick={()=>this.deleteAuction(auction.auctionProduct.id)}>Delete</Button>
-                                                            </td>
-                                                            <td>
-                                                                <Button variant="outline-success" size="sm" className={"text-center"} onClick={()=>this.showAuctionDetails(auction.auctionProduct.id)}>View</Button>
-                                                            </td>
-                                                        </tr>
-                                                    )
-                                                })
-                                            }
-                                            </tbody>
-                                        </Table>
-                                    </Card.Body>
-                                </Card>
-                            </div>
+                                                <td colSpan={7} className={"text-center"}>No saved auctions</td>
+                                            </tr> :
+                                            savedAuctions.map((auction, index) => {
+                                                return (
+                                                    <tr key={auction.auctionProduct.id}>
+                                                        <td>{index + 1}</td>
+                                                        <td>{auction.auctionProduct.product_name}</td>
+                                                        <td>{auction.auctionProduct.owner.firstName}</td>
+                                                        <td>{auction.auctionProduct.max_bid}</td>
+                                                        <td>{auction.auctionProduct.auction_start_date}</td>
+                                                        <td>{auction.auctionProduct.auction_end_date}</td>
+                                                        <td>
+                                                            <Button variant="outline-danger" size="sm" className={"text-center"} onClick={()=>deleteAuction(auction.auctionProduct.id)}>Delete</Button>
+                                                        </td>
+                                                        <td>
+                                                            <Button variant="outline-success" size="sm" className={"text-center"} onClick={()=>showAuctionDetails(auction.auctionProduct.id)}>View</Button>
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            })
+                                        }
+                                        </tbody>
+                                    </Table>
+                                </Card.Body>
+                            </Card>
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
 
+
+
+
+}
 export default SavedAuctions;
