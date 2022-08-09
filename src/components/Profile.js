1f0473fd87c215d40940646ea42df329359b9e08
Sadia Saman <1705102@ugrad.cse.buet.ac.bd>
2022-08-09 17:50:36 +0600

    profile page complete

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,8 @@ import InputGroup from "react-bootstrap/InputGroup";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import {BsFillBellFill} from "react-icons/bs";
+import "./Card.css"
 
 
 function Profile () {
@@ -11,15 +13,7 @@ function Profile () {
     const saveChanges = () => {
         alert("clicked")
     }
-
-    const padding_top ={
-        paddingTop: '10px'
-    }
-
-    const padding_top_bottom_between_text ={
-        paddingTop: '10px',
-        paddingBottom: '10px'
-    }
+ 
 
 
     return (
@@ -32,22 +26,58 @@ function Profile () {
                         <Container>
                             <Row xs={1} md={2}>
                                 <Col>
-                                    <div style={{borderRadius: '50% !important', backgroundColor: '#f5f5f5', width: '150px', height: '150px', margin: 'auto'}}>
+                                    <div style={{ borderRadius: '25% !important',backgroundColor: '#f5f5f5', width: '150px', height: '150px'}}>
                                         <img
-                                            src={require("../images/login-auction.jpg")}
+                                            src={require("../images/man-profile.webp")}
                                             alt="online-auctions photo"
                                             height={'100%'}
                                             width={'100%'}
                                         />
+                                        <div className="text-container">
+                                            <p className="profile-tile"> 
+                                                Name<br></br>
+                                                Email<br></br> 
+                                                Address<br></br> 
+                                                Contact Number<br></br> 
+                                            </p>
+                                        </div>
                                     </div>
                                 </Col>
-                                <Col>2 of 3</Col>
-                                <Col>3 of 3</Col>
+                                <Col>
+                                    <div class="form-group"> 
+                                        <div className="input-container">
+                                            <label for ="name" >User Name </label>
+                                            <input type ="text" class="form-control textarea" id="name" placeholder="change user name"></input>
+                                        </div>
+                                        <div className="input-container">
+                                            <label for ="email" >User Email </label>
+                                            <input type="email" class="form-control textarea" id="email" placeholder="change Email"></input>
+                                        </div>
+                                        <div className="input-container">
+                                            <label for ="password" >Change your Password </label>
+                                            <input type="password" class="form-control textarea" id="password" placeholder="change Password"></input>
+                                        </div>
+
+                                        <div className="d-grid gap-2 col-6 mx-auto text-container">
+                                            <button type="submit" className="btn btn-danger" >Save Profile</button>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col>
+                                    <div class=""> 
+                                        <div className="input-container">
+                                            <label>Do you like get email about ongoing auctions?<br/>Click the Bell <BsFillBellFill type="submit" /></label>
+                                        </div>
+                                        <div className="input-container">
+                                            <label>Do you like to get promotional mail? Click <button type="button" class="btn btn-outline-warning">Yes</button> ! </label> 
+
+                                        </div> 
+                                    </div></Col>
                             </Row>
                         </Container>
-                        <div className="d-grid gap-2 col-6 mx-auto text-container" >
+                        {/* <div className="d-grid gap-2 col-6 mx-auto text-container" >
                             <button type="submit" className="btn btn-info text-white" onClick={saveChanges}>Save Changes</button>
-                        </div>
+                        </div> */}
 
                     </Card.Body>
                 </Card>
