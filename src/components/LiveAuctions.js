925c3d3697802ea1f99527470916b43592976e6b
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2022-08-12 01:37:26 +0600

    Live auction page showing all live auctions

--- src/components/LiveAuctions.js
+++ src/components/LiveAuctions.js
@@ -1,80 +1,107 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import {Card} from "react-bootstrap"; 
 import './Card.css'
 import axios from "axios"; 
-import {Route,Router,Link, Routes} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import { useEffect } from 'react';
-import LiveAuction from './LiveAuction';
 
-class LiveAuctions extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            live_auctions: []
-        }
-    }
+import {toast} from "react-toastify";
+import {Button} from "react-bootstrap";
 
-    componentDidMount() {
-        axios.get("http://localhost:8080/liveAuctions/get/all")
-            .then(response =>response.data)
-            .then((data)=>{
-                this.setState({live_auctions: data});
+function LiveAuctions (){
+
+    let user_id = localStorage.getItem('user_id');
+    const  [auctions, setAuctions] = useState([]);
+    const navigate = useNavigate();
+
+
+    const fetchLiveAuctions = () => {
+        let url = "http://localhost:8080/auction_products/all/ongoing/user/" + user_id;
+
+        axios.get(url).then(r => {
+            setAuctions(r.data);
+        }).catch(e => {
+            toast.error("Error fetching auctions");
         })
+
     }
 
-    
-    render(){
-        return (
-            <div className="home-element-padding">
-            <div className="card-container">
-                <div className='container-fluid' >
-                    <div className="row">
-                        <div className=" col-sm-12">
-                            <Card className=" bg-warning.bg-gradient">
-                                <Card.Header className={"bg-warning text-white text-center"}> Live Auctions  </Card.Header>
-                                <Card.Body>
-                                     {this.state.live_auctions.length === 0 ?
-                                        <h3>
-                                            <p colSpan={7} className={"text-center"}>No Live Auction available</p>
-                                        </h3> :
-                                        this.state.live_auctions.map((auction, index) => {
-                                            return (
-                                                
-                                                <Link to={{
-                                                    pathname : "/liveAuctions/liveAuction",
-                                                    state : {'auction':auction} 
+    const saveAuction = (auction_id) => {
+        let url = "http://localhost:8080/savedAuctions/create/user/" + user_id + "/auction/" + auction_id + "/" + localStorage.getItem('user');
+        axios({
+            method: 'post',
+            url: url,
+            headers: {},
+            data: {
+                date: new Date(),
+            }
+        }).then(response => {
+            if (response.data!=null){
+                if (response.status === 200){
+                    toast.success("Auction added to saved auctions");
+                    document.getElementById("save").disabled = true;
+                }
+            }
+        }).catch(error => {
+            console.log(error);
+            toast.error("Error adding auction to saved auctions");
+            document.getElementById("save").disabled = true;
+        });
 
-                                                }}> 
-                                                    <div className='card-image-container border border-warning'>
-                                                        <img
-                                                                src={require('../images/vase.jpeg')}
-                                                                alt="product image"
-                                                                height={200}
-                                                                width={150}
-                                                            />
-                                                        <div className="image-desc-container">
-                                                        <p>{auction.auctionProduct.product_name}</p>
-                                                                <p>{auction.auctionProduct.max_bid}</p>
-                                                                <p>{auction.auctionProduct.auction_start_date}</p>
-                                                                <p>{auction.auctionProduct.auction_end_date}</p>
-
-                                                        </div>
-
-                                                    </div>
-                                                    <LiveAuction auction={auction}/>
-                                                </Link>
-                                                )
-                                            })
-                                        } 
-                                </Card.Body>
-                                
-                            </Card>                        
-                        </div> 
-                    </div> 
-                </div> 
-            </div></div>
-        )
     }
+
+    const viewAndBid = (auction_id) => {
+        alert("View and Bid");
+    }
+
+    useEffect(() => {
+        fetchLiveAuctions();
+    },[]);
+
+    return (
+        <div className="home-element-padding">
+        <div className="card-container">
+            <div className='container-fluid' >
+                <div className="row">
+                    <div className=" col-sm-12">
+                        <Card className=" bg-warning.bg-gradient">
+                            <Card.Header className={"bg-warning text-white text-center"}> Live Auctions </Card.Header>
+                            {auctions.length === 0 ? <Card.Body> No live auctions </Card.Body> :
+                                auctions.map(auction => {
+                                    return(
+                                        <Card.Body key={auction.id}>
+                                            <div className='card-image-container border border-warning'>
+                                                <img
+                                                    src={require('../images/vase.jpeg')}
+                                                    alt="product image"
+                                                    height={200}
+                                                    width={150}
+                                                />
+                                                <div className="image-desc-container">
+                                                    <p>Product name : {auction.product_name}</p>
+                                                    <p>Max Bid: {auction.max_bid}</p>
+                                                    <p>Start Date: {auction.auction_start_date}</p>
+                                                    <p>End Date: {auction.auction_end_date}</p>
+
+                                                    <Button variant="primary" onClick={()=>saveAuction(auction.id)} id={"save"}>Save</Button>{' '}
+                                                    <Button variant="secondary" onClick={()=>viewAndBid(auction.id)} id={"view"}>View and Bid</Button>{' '}
+
+                                                </div>
+
+
+
+                                            </div>
+                                        </Card.Body>
+                                    );
+                                })
+                            }
+
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        </div></div>
+    );
 }
 
 export default LiveAuctions
