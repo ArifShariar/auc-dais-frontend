2ca6f7999110a692c2b1f506773340f65dcc3452
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2022-08-12 01:38:06 +0600

    renamed "class" to "className" in PaymentWithBkash.js and PaymentWithBkash.js

--- src/components/PaymentWithBkash.js
+++ src/components/PaymentWithBkash.js
@@ -5,28 +5,28 @@ function PaymentWithBkash() {
     <div className='payment-method'>
         <h3 className='text-center'> Payment with Bkash</h3>
         <form className='payment-method'>
-            <div class="form-group d-flex flex-row justify-content-between">
-                <input type="number" class="form-control rounded-pill" id="phonenumber" placeholder="Phone Number"/> 
+            <div className="form-group d-flex flex-row justify-content-between">
+                <input type="number" className="form-control rounded-pill" id="phonenumber" placeholder="Phone Number"/>
             </div>
-            <div class="form-group d-flex flex-row justify-content-between"> 
-                <input type="text" class="form-control rounded-pill" id="trx_id" aria-describedby="name" placeholder="Trx Number"/>  
+            <div className="form-group d-flex flex-row justify-content-between">
+                <input type="text" className="form-control rounded-pill" id="trx_id" aria-describedby="name" placeholder="Trx Number"/>
             </div>
-            <div class="form-group d-flex flex-row justify-content-between"> 
-                <input type="text" class="form-control rounded-pill" id="amount" aria-describedby="name" placeholder="amount"/> 
+            <div className="form-group d-flex flex-row justify-content-between">
+                <input type="text" className="form-control rounded-pill" id="amount" aria-describedby="name" placeholder="amount"/>
             </div>
-            <div class="form-group d-flex flex-row justify-content-between"> 
-                <input type="password" class="form-control rounded-pill" id="pin" placeholder="Enter Pin"/> 
+            <div className="form-group d-flex flex-row justify-content-between">
+                <input type="password" className="form-control rounded-pill" id="pin" placeholder="Enter Pin"/>
             </div>
             <hr/> 
             <hr></hr>
-            <div class="message-container">
+            <div className="message-container">
                 <h4>Payment Details</h4>
-                <div class="form-group d-flex flex-row justify-content-between"> 
+                <div className="form-group d-flex flex-row justify-content-between">
                     <p>Account type</p> 
                     <p> account type cost</p>              
                 </div>
                 <hr></hr>
-                <div class="form-group d-flex flex-row justify-content-between"> 
+                <div className="form-group d-flex flex-row justify-content-between">
                     <p>total</p> 
                     <p>100000000000 $</p>              
                 </div>
