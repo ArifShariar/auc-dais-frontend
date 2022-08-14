ed76d62b81c76074b4fac47e51987ed77b8eae72
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2022-08-15 00:42:17 +0600

    Fixed indentation

--- src/components/PaymentWithPayPal.js
+++ src/components/PaymentWithPayPal.js
@@ -1,57 +1,57 @@
 import React from 'react'
 
 function PaymentWithPayPal() {
-  return (
-    <div className='payment-method'>
-        <h3 className='text-center'> Payment with PayPal</h3>
-        <form className='payment-method'>
-            <div className="form-group d-flex flex-row justify-content-between">
-                <input type="name" className="form-control rounded-pill" id="name" aria-describedby="name" placeholder="First Name"/>
-                <input type="name" className="form-control rounded-pill" id="name" aria-describedby="name" placeholder="Last Name"/>
-            </div>
-            <div className="form-group d-flex flex-row justify-content-between">
-                <input type="email" className="form-control rounded-pill" id="email" placeholder="Email"/>
-                <input type="number" className="form-control rounded-pill" id="phonenumber" placeholder="Phone Number"/>
-            </div>
-            <hr/>
-
-            <h5>Card Details</h5>
-            <div className="form-group d-flex flex-row justify-content-between">
-                
-                <input type="number" className="form-control rounded-pill" id="phonenumber" placeholder="Card Number"/>
-                <input
-                    type="text"
-                    className="form-control rounded-pill"
-                    onFocus={
-                        (e)=> {
-                            e.currentTarget.type = "date";
-                            e.currentTarget.placeholder = "Expiration date in dd/mm/yyyy"
-                            e.currentTarget.focus();  
-                        }
-                    }
-                    placeholder="Expiration date"
-                />
-                <input type="number" className="form-control rounded-pill" id="cvv2" placeholder="cvv"/>               
-            </div> 
-            <hr></hr>
-            <div className="message-container">
-                <h4>Payment Details</h4>
+    return (
+        <div className='payment-method'>
+            <h3 className='text-center'> Payment with PayPal</h3>
+            <form className='payment-method'>
                 <div className="form-group d-flex flex-row justify-content-between">
-                    <p>Account type</p> 
-                    <p> account type cost</p>              
+                    <input type="name" className="form-control rounded-pill" id="name" aria-describedby="name" placeholder="First Name"/>
+                    <input type="name" className="form-control rounded-pill" id="name" aria-describedby="name" placeholder="Last Name"/>
+                </div>
+                <div className="form-group d-flex flex-row justify-content-between">
+                    <input type="email" className="form-control rounded-pill" id="email" placeholder="Email"/>
+                    <input type="number" className="form-control rounded-pill" id="phonenumber" placeholder="Phone Number"/>
+                </div>
+                <hr/>
+
+                <h5>Card Details</h5>
+                <div className="form-group d-flex flex-row justify-content-between">
+
+                    <input type="number" className="form-control rounded-pill" id="phonenumber" placeholder="Card Number"/>
+                    <input
+                        type="text"
+                        className="form-control rounded-pill"
+                        onFocus={
+                            (e)=> {
+                                e.currentTarget.type = "date";
+                                e.currentTarget.placeholder = "Expiration date in dd/mm/yyyy"
+                                e.currentTarget.focus();
+                            }
+                        }
+                        placeholder="Expiration date"
+                    />
+                    <input type="number" className="form-control rounded-pill" id="cvv2" placeholder="cvv"/>
                 </div>
                 <hr></hr>
-                <div className="form-group d-flex flex-row justify-content-between">
-                    <p>total</p> 
-                    <p>100000000000 $</p>              
+                <div className="message-container">
+                    <h4>Payment Details</h4>
+                    <div className="form-group d-flex flex-row justify-content-between">
+                        <p>Account type</p>
+                        <p> account type cost</p>
+                    </div>
+                    <hr></hr>
+                    <div className="form-group d-flex flex-row justify-content-between">
+                        <p>total</p>
+                        <p>100000000000 $</p>
+                    </div>
                 </div>
-            </div>
-             <div className="d-grid gap-2 col-6 mx-auto text-container" >
-                <button type="submit" className="btn btn-danger">Confirm Payment</button>
-            </div>
-        </form>
-    </div>
-  )
+                <div className="d-grid gap-2 col-6 mx-auto text-container" >
+                    <button type="submit" className="btn btn-danger">Confirm Payment</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default PaymentWithPayPal
