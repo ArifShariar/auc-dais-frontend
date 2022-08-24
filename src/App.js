7dd3260de8c75ad9f33c2680c23fcd6973969809
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2022-08-24 16:16:18 +0600

    Added mapbox map in /map, check it out

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ import UpdateAuction from "./components/UpdateAuction";
 import ShowWonAuctionDetails from "./components/ShowWonAuctionDetails";
 import ShowMyAuctionDetails from "./components/ShowMyAuctionDetails";
 import {AuctionStats} from "./components/AuctionStats";
+import Map from "./components/Map";
 
 // We may need to get rid of RequireAuth and use PersistLogin instead
 toast.configure();
@@ -70,6 +71,7 @@ function App() {
             <Route path="/wonAuctions/payment" element={<Payment/>} />
             <Route path="/settings" element={<UserSettings/>} />
             <Route path="/email_verification" element={<VerifyEmail/>}/>
+            <Route path="/map" element={<Map/>}/>
             <Route path="/" element={<Home />} />
             <Route path="/statistics/:id" element={<AuctionStats/>} />
         </Routes>
